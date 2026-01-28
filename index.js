ec4aa820084e41ea7a5a495abb824daeba39482b
1-tent <thitipat.naro@sbac.ac.th>
2026-01-28 15:39:55 +0700

    V2

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
+
+let department = [
+    { id: 1, name: "IT" },
+    { id: 2, name: "DBT" }
+];
+
 app.get("/", (req , res) => {
     res.send('Miss.Mintra Ritidet');
 });
@@ -10,3 +17,15 @@ app.listen(PORT, () => {
     console.log("Server Starting on port", PORT);
 });
 
+app.get("/department", (req, res) => {
+    res.send(department);
+});
+
+app.post("/department", (req, res) => {
+ let newDepartment = {
+    id: department.length + 1,
+    name: req.body.name
+ };
+    department.push(newDepartment);
+    res.send(department);
+});
